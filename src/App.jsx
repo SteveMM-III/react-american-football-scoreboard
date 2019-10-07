a794fa579f0b5c09ad2ca6eaf2c3cdb560ac004a
Kronykus <MoreheadSteve@gmail.com>
2019-10-07 15:54:28 -0500

    scores

--- src/App.jsx
+++ src/App.jsx
@@ -38,11 +38,11 @@ function App() {
       <section className="buttons">
         <div className="homeButtons">
           {/* TODO STEP 4 - Now we need to attach our state setter functions to click listeners. */}
-          <button onClick={ () => setScore( homeName, 6) } className="homeButtons__touchdown">Home Touchdown</button>
+          <button onClick={ () => setScore( homeName, 7) } className="homeButtons__touchdown">Home Touchdown</button>
           <button onClick={ () => setScore( homeName, 3) } className="homeButtons__fieldGoal">Home Field Goal</button>
         </div>
         <div className="awayButtons">
-          <button onClick={ () => setScore( awayName, 6) } className="awayButtons__touchdown">Away Touchdown</button>
+          <button onClick={ () => setScore( awayName, 7) } className="awayButtons__touchdown">Away Touchdown</button>
           <button onClick={ () => setScore( awayName, 3) } className="awayButtons__fieldGoal">Away Field Goal</button>
         </div>
       </section>
