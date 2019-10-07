9582082bba81837806385d0fbca09475aeb72663
Kronykus <MoreheadSteve@gmail.com>
2019-10-07 16:41:40 -0500

    moved toprow from App to own file

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 //TODO: STEP 1 - Import the useState hook.
 import React, { useState } from "react";
 import "./App.css";
+import TopRow from "./TopRow";
 import BottomRow from "./BottomRow";
 
 function App() {
@@ -19,20 +20,7 @@ function App() {
   return (
     <div className="container">
       <section className="scoreboard">
-        <div className="topRow">
-          <div className="home">
-            <h2 className="home__name">{ homeName }</h2>
-
-            {/* TODO STEP 3 - We need to change the hardcoded values in these divs to accept dynamic values from our state. */}
-
-            <div className="home__score">{ home }</div>
-          </div>
-          <div className="timer">00:03</div>
-          <div className="away">
-            <h2 className="away__name">{ awayName }</h2>
-            <div className="away__score">{ away }</div>
-          </div>
-        </div>
+        <TopRow home={home} away={away} homeName={homeName} awayName={awayName} />
         <BottomRow />
       </section>
       <section className="buttons">
