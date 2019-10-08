90759be02a2022456ea67bcafc3eef7a78282244
Kronykus <MoreheadSteve@gmail.com>
2019-10-07 22:51:17 -0500

    moved buttons from App to Buttons.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import React, { useState } from "react";
 import "./App.css";
 import TopRow from "./TopRow";
 import BottomRow from "./BottomRow";
+import Buttons from "./Buttons";
 
 function App() {
   //TODO: STEP 2 - Establish your applictaion's state with some useState hooks.  You'll need one for the home score and another for the away score.
@@ -28,18 +29,7 @@ function App() {
         <TopRow home={home} away={away} homeName={homeName} awayName={awayName} />
         <BottomRow />
       </section>
-      <section className="buttons">
-        <div className="homeButtons">
-          {/* TODO STEP 4 - Now we need to attach our state setter functions to click listeners. */}
-          <button onClick={ () => setScore( homeName, 7) } className="homeButtons__touchdown">Home Touchdown</button>
-          <button onClick={ () => setScore( homeName, 3) } className="homeButtons__fieldGoal">Home Field Goal</button>
-        </div>
-        <div className="awayButtons">
-          <button onClick={ () => setScore( awayName, 7) } className="awayButtons__touchdown">Away Touchdown</button>
-          <button onClick={ () => setScore( awayName, 3) } className="awayButtons__fieldGoal">Away Field Goal</button>
-        </div>
-        <button onClick={ () => reset() }>Reset</button>
-      </section>
+      <Buttons homeName={homeName} awayName={awayName} setScore={setScore} reset={reset} />
     </div>
   );
 }
